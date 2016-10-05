7418d3e80101a59376d7bfcd0edf732b360f7a85
Dylan Ratcliffe <dylan.ratcliffe@puppet.com>
2016-10-05 22:07:37 +1100

    Added active instances tuning

--- manifests/site.pp
+++ manifests/site.pp
@@ -8,6 +8,11 @@ node /clamps\d\.scaling\.puppetconf\.com/ {
 
 node 'master.scaling.puppetconf.com' {
   include clamps::master
+
+  Pe_hocon_setting <| title == 'jruby-puppet.max-active-instances' |> {
+    ensure => present,
+    value  => 8,
+  }
 }
 
 node /user\d*\.clamps\d\.scaling\.puppetconf\.com/ {
